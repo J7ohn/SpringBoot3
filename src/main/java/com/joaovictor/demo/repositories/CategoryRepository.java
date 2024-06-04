989212cf060e7237caf1cc2a86353d0fee3eff80
J7ohn <jvfonseca1349@gmail.com>
2024-06-04 01:49:27 -0300

    Category

---
+++ src/main/java/com/joaovictor/demo/repositories/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.joaovictor.demo.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.joaovictor.demo.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+	
+	
+}
